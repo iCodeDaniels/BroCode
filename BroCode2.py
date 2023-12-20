7fe668a82171a95003849e5d66f149052c826e24
iCodeDaniels <akinloyeifeoluwa46@gmail.com>
2023-12-20 14:13:35 +0100

    Update BroCode2.py

--- BroCode2.py
+++ BroCode2.py
@@ -21,6 +21,7 @@ while command != "quit":
 Start ---- Press the start button to put on the car
 Stop ---- Press the stop button to put off the car
 Quit ---- Quit brings the car to a total stop...
+History ---- The car came into existence with the aim of pulling off the ground
         ''')
         break
 else:
