37ab5280edcdbf61a7a154132e16616d06cf581a
iCodeDaniels <akinloyeifeoluwa46@gmail.com>
2024-04-12 22:44:16 +0100

    updated code

--- WorkSpace.py
+++ WorkSpace.py
@@ -1,17 +1,12 @@
 from tkinter import *
-from tkinter import messagebox
-
-def proceed():
-    #messagebox.showinfo(title="Regulated Questions",message="This is a step to me becoming a great software developer")
-    #messagebox.showwarning(title="Really risky on this",message="This is really a bad choice to make")
-    messagebox.showerror(title="Error calling message",message="There's an error with the processed data")
+from tkinter import colorchooser
 
 window = Tk()
 
-photo = PhotoImage(file='coder.png')
-window.iconphoto(True,photo)
+window.geometry("300x350")
+window.config(bg="cyan")
 
-button = Button(window,text="Click me",command=proceed)
-button.pack()
+color_button = Button(window,text="Choose your color")
+color_button.pack()
 
 window.mainloop()
